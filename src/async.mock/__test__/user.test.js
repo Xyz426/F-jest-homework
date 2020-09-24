6d84ffc377156e9d4a68360e4995682d8ccda919
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-24 17:14:10 +0800

    [mengqiu]  add feedback

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -6,16 +6,20 @@ jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
     axios.post = jest.fn().mockResolvedValue({ data: "good" });
-    return expect(register("", "")).resolves.toBe("good");
+    // TODO feedback: 建议使用async/await哈
+    // return expect(register("", "")).resolves.toBe("good");
+    await expect(register("", "")).resolves.toBe("good");
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
     verifyUsername.mockReturnValue(false);
     const result = register("", "");
-    return expect(result).rejects.toThrow("wrong username or password");
+    // return expect(result).rejects.toThrow("wrong username or password");
+    // TODO feedback: 建议使用async/await哈
+    await expect(result).rejects.toThrow("wrong username or password");
   });
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -2,11 +2,25 @@ import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
 
+// jest.mock("../recipient", () => {
+//   return jest.fn().mockImplementation(() => ({
+//     hasAntibodies: false,
+//     acceptInjection: jest.fn(),
+//   }));
+// });
+
+// TODO feedback: 我们应该定义一个需要mock的方法的变量，这样后面再test()里面就都可以使用这个变量了
+const mockAcceptInjection = jest.fn();
+const mockGetHasAntibodies = jest.fn(() => false);
+
 jest.mock("../recipient", () => {
-  return jest.fn().mockImplementation(() => ({
-    hasAntibodies: false,
-    acceptInjection: jest.fn(),
-  }));
+  return jest.fn().mockImplementation(() => {
+    return {
+      acceptInjection: mockAcceptInjection,
+      // TODO 我们应该去mock这个class的function,而不是去mock这个class的field,所以我们应该去mock getHasAntibodies()这个方法
+      getHasAntibodies: mockGetHasAntibodies,
+    };
+  });
 });
 
 beforeEach(() => {
@@ -19,7 +33,11 @@ describe("inject", () => {
     // TODO 14: add test here
     const vaccineTest = new VaccineTest();
     vaccineTest.inject();
-    expect(vaccineTest.recipient.acceptInjection).toHaveBeenCalledWith(
+    // expect(vaccineTest.recipient.acceptInjection).toHaveBeenCalledWith(
+    //   expect.any(Covid19Vaccine)
+    // );
+
+    expect(mockAcceptInjection).toHaveBeenCalledWith(
       expect.any(Covid19Vaccine)
     );
   });
@@ -29,14 +47,18 @@ describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
     const vaccineTest = new VaccineTest();
-    vaccineTest.recipient.getHasAntibodies = jest.fn().mockReturnValue(true);
+    // vaccineTest.recipient.getHasAntibodies = jest.fn().mockReturnValue(true);
+    // TODO feedback: 这里我们就可以使用之前定义好的变量了
+    mockGetHasAntibodies.mockImplementation(() => true);
     expect(vaccineTest.test()).toBe("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
     const vaccineTest = new VaccineTest();
-    vaccineTest.recipient.getHasAntibodies = jest.fn().mockReturnValue(false);
+    // vaccineTest.recipient.getHasAntibodies = jest.fn().mockReturnValue(false);
+    // TODO feedback: 这里我们就可以使用之前定义好的变量了
+    mockGetHasAntibodies.mockImplementation(() => false);
     expect(vaccineTest.test()).toBe("Vaccine Test Failed");
   });
 });
